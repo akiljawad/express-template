32e963d6cf8bac4c08a95522a7f249d191b48469
akiljawad <akil.jawad976@gmail.com>
2025-05-31 21:03:51 +0600

    feat: project setup
    Changes:
    1. Install dependency -> helmet, cors, bodyParser
    2. Update in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, {Request, Response} from 'express';
+import helmet from "helmet";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
