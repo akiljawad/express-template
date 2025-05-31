dfbab93d021890d6822904139e6f0b2273b35793
akiljawad <akil.jawad976@gmail.com>
2025-06-01 01:25:20 +0600

    setup: constants
    Changes:
    1. Add messages.ts file for central messages
    2. Add status-code.ts for status-code parameters

---
+++ src/common/constants/messages.ts
@@ -0,0 +1,9 @@
+export const Messages = {
+    INTERNAL_ERROR: 'Something went wrong. Please try again later.',
+    BAD_REQUEST_ERROR: 'Bad request. Please check your request parameters.',
+    NOT_FOUND_ERROR: 'Not found.',
+    FORBIDDEN_ERROR: 'Forbidden.',
+    UNAUTHORIZED_ERROR: 'Unauthorized.',
+    TOO_MANY_REQUESTS_ERROR: 'Too many requests. Please try again later.',
+    INVALID_TOKEN: 'Invalid or expired token.',
+}
